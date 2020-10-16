ce7f37581dc864dadaae672977705ad4336b11e6
Gonzalo Campos <erick.campos@wizeline.com>
2020-10-15 19:56:33 -0500

    Add React.StrictMode notes.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import SearchParams from './SearchParams';
 
+// <React.StringMode> will restrict the use
+// of older or unstable api.
 const App = () => (
   <div>
     <h1>Adopt Me</h1>
