9beb5507f0d72c5d4db8cf23ad38900657c1cd5d
Gonzalo Campos <erick.campos@wizeline.com>
2020-10-17 18:53:34 -0500

    Create a theme using createContext hook.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
 import SearchParams from './SearchParams';
 import { Link, Router } from '@reach/router';
 import Details from './Details';
+import ThemeContext from './ThemeContext';
 
 // <React.StringMode> will restrict the use
 // of older or unstable api.
 // Reach router will always render the most
 // specific route.
-const App = () => (
-  <div>
-    <header>
-      <Link to="/">Adopt Me</Link>
-    </header>
-    <Router>
-      <SearchParams path="/" />
-      <Details path="/details/:id" />
-    </Router>
-  </div>
-);
+const App = () => {
+  const themeHook = useState('darkBlue');
 
+  return (
+    <ThemeContext.Provider value={themeHook}>
+      <div>
+        <header>
+          <Link to="/">Adopt Me</Link>
+        </header>
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
+      </div>
+    </ThemeContext.Provider>
+  );
+};
 render(<App />, document.getElementById('root'));
