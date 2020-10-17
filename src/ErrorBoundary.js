57f987d29f8b37b6eac679523fd8adc2393b4cb9
Gonzalo Campos <erick.campos@wizeline.com>
2020-10-17 18:41:30 -0500

    Make ErrorBoundary redirecto the user to home after 5 seconds.

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -1,7 +1,7 @@
 // mostly code from react.js/docs/error-boundaries.html
 
 import React, { Component } from 'react';
-import { Link } from '@reach/router';
+import { Link, Redirect } from '@reach/router';
 
 class ErrorBoundary extends Component {
   state = {
@@ -19,7 +19,24 @@ class ErrorBoundary extends Component {
     console.error('ErrorBoundary caught an error', error, info);
   }
 
+  // will run everytime it gets new state or new props
+  componentDidUpdate() {
+    if (this.state.hasError) {
+      setTimeout(
+        () =>
+          this.setState({
+            redirect: true,
+          }),
+        5000
+      );
+    }
+  }
+
   render() {
+    if (this.state.redirect) {
+      return <Redirect to="/" />;
+    }
+
     if (this.state.hasError) {
       return (
         <h1>
