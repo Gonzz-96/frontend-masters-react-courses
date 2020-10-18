9eadc038efbc30a3483bffc78b557b08e4962c6b
Gonzalo Campos <erick.campos@wizeline.com>
2020-10-18 14:37:59 -0500

    Playing with crazy inline CSS in Javascript.

--- NavBar.js
+++ NavBar.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from '@reach/router';
 import { css } from '@emotion/core';
 
-const NavBar = () => (
+const color = 'black';
+
+// anything that can be done with javascript
+// can be done in inline emotion CSS.
+const NavBar = () => {
+  const [padding, setPadding] = useState(15);
+
+  return (
+  // eslint-disable-next-line
   <header
+    onClick={() => setPadding(padding + 15)}
     css={css`
-      background-color: #333;
-      padding: 15px;
+      background-color: ${color};
+      padding: ${padding}px;
     `}
   >
     <Link to="/">Adopt Me!</Link>  
-    <span role="img" aria-label="logo">🐶</span>
+    <span css={css`
+      font-size: 60px;
+    `}
+      role="img" 
+      aria-label="logo">🐶</span>
   </header>
-);
+)};
 
 export default NavBar;
