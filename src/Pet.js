3ac4bf0875dfc1ed3b08c496de833d8c2026ba77
Gonzalo Campos <erick.campos@wizeline.com>
2020-10-17 19:49:46 -0500

    Overwritting the theme.

--- src/Pet.js
+++ src/Pet.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from '@reach/router';
 
 const Pet = ({ name, animal, breed, media, location, id }) => {
   let hero = 'http://placecorgi.com/300/300';
@@ -7,8 +8,11 @@ const Pet = ({ name, animal, breed, media, location, id }) => {
     hero = media[0].small;
   }
 
+  // anchor tag was replace by Link because it caused the
+  // destruction of the whole DOM, that required a whole re-rendering
+  // of the application, and hence destroying the context.
   return (
-    <a href={`/details/${id}`} className="pet">
+    <Link to={`/details/${id}`} className="pet">
       <div className="image-container">
         <img src={hero} alt={name} />
       </div>
@@ -16,7 +20,7 @@ const Pet = ({ name, animal, breed, media, location, id }) => {
         <h1>{name}</h1>
         <h2>{`${animal} - ${breed} - ${location}`}</h2>
       </div>
-    </a>
+    </Link>
   );
 };
 
