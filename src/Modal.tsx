d4589c059e636c06a8e2f6d0f54f96d333230150
Gonzalo Campos <erick.campos@wizeline.com>
2020-10-18 21:26:42 -0500

    Migrate Modal.js to Modal.tsx

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -1,22 +1,24 @@
-import React, { useEffect, useRef } from 'react';
+import React, { FunctionComponent, useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 
-const Modal = ({ children }) => {
+const Modal: FunctionComponent = ({ children }) => {
   // create a single reference of the passed object
-  const elRef = useRef(null);
-
-  if (!elRef.current) {
-    const div = document.createElement('div');
-    elRef.current = div;
-  }
+  const elRef = useRef(document.createElement('div'));
 
   useEffect(() => {
     const modalRoot = document.getElementById('modal');
+
+    if (!modalRoot) {
+      return;
+
+    }
     modalRoot.appendChild(elRef.current);
 
     // this is the cleanup function
     // this is run when the component is unmount
-    return () => modalRoot.removeChild(elRef.current);
+    return () => {
+      modalRoot.removeChild(elRef.current)
+    };
   }, []);
 
   return createPortal(<div>{children}</div>, elRef.current);
